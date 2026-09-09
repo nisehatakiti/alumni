932b1a7a8d9897f847081cd1f750f51ca625badf
nisehatakiti <nisehatakiti@gmail.com>
2026-09-10 07:29:13 +0900

    feat: load person greeting order admin page

--- alumni-core/includes/class-plugin.php
+++ alumni-core/includes/class-plugin.php
@@ -112,6 +112,7 @@ final class Plugin {
 		require_once ALUMNI_CORE_PATH . 'admin/pages/class-homepage-page.php';
 		require_once ALUMNI_CORE_PATH . 'admin/pages/class-menu-page.php';
 		require_once ALUMNI_CORE_PATH . 'admin/pages/class-org-chart-page.php';
+		require_once ALUMNI_CORE_PATH . 'admin/pages/class-person-greeting-order-page.php';
 
 		require_once ALUMNI_CORE_PATH . 'includes/modules/news-events/class-post-type.php';
 		require_once ALUMNI_CORE_PATH . 'includes/modules/news-events/class-listing-shortcode.php';
