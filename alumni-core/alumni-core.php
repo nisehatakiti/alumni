f28e652c99064574c5b25f72ba21f8f8b2677602
nisehatakiti <nisehatakiti@gmail.com>
2026-09-12 07:38:07 +0900

    Fix Facebook and X SNS window rendering
    * Fix SNS provider rendering and external widget timing
    
    * Fix SNS provider rendering and external widget timing
    
    * Fix SNS provider rendering and external widget timing
    
    * Fix SNS provider rendering and external widget timing
    
    * Fix SNS provider rendering and external widget timing

--- alumni-core/alumni-core.php
+++ alumni-core/alumni-core.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Alumni Core
  * Plugin URI:         https://nisehatakiti.online/alumni/
  * Description:       同窓会ホームページパッケージの共通データ基盤。設定管理、卒業期計算などの基盤機能を提供します。
- * Version:            0.1.15
+ * Version:            0.1.16
  * Requires at least: 5.9
  * Requires PHP:       7.4
  * Author:              nisehatakiti
@@ -21,7 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Basic plugin constants. Prefixed to avoid collisions with other plugins/themes.
-define( 'ALUMNI_CORE_VERSION', '0.1.15' );
+define( 'ALUMNI_CORE_VERSION', '0.1.16' );
 define( 'ALUMNI_CORE_DB_VERSION', '1' );
 define( 'ALUMNI_CORE_FILE', __FILE__ );
 define( 'ALUMNI_CORE_PATH', plugin_dir_path( __FILE__ ) );
--- alumni-theme/functions.php
+++ alumni-theme/functions.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'ALUMNI_THEME_VERSION', '0.1.4' );
+define( 'ALUMNI_THEME_VERSION', '0.1.5' );
 define( 'ALUMNI_THEME_DIR', get_template_directory() );
 define( 'ALUMNI_THEME_URI', get_template_directory_uri() );
 
